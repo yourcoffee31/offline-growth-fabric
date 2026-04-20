70b5c97eb1cb5af73b1935ba20e8d33c930706ab
yourcoffee31 <yourcoffee265@gmail.com>
2026-04-20 14:58:18 +0300

    Update OfflineGrowthSavedData.java

--- src/main/java/ru/floweykill27/offlinegrowth/OfflineGrowthSavedData.java
+++ src/main/java/ru/floweykill27/offlinegrowth/OfflineGrowthSavedData.java
@@ -1,79 +1,51 @@
 package ru.floweykill27.offlinegrowth;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.Tag;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
-import net.minecraft.world.level.storage.DimensionDataStorage;
+import net.minecraft.world.level.saveddata.SavedDataType;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public final class OfflineGrowthSavedData extends SavedData {
-    private static final String SAVE_ID = OfflineGrowthMod.MOD_ID;
+    private static final String SAVE_ID = OfflineGrowthMod.MOD_ID + "_offline_growth";
     private static final String CHUNKS_TAG = "chunks";
     private static final String ENTITIES_TAG = "entities";
-    private static final String ENTITY_ID_TAG = "id";
-    private static final String TIME_TAG = "time";
+
+    private static final Codec<Map<Long, Long>> LONG_LONG_MAP_CODEC = Codec.unboundedMap(
+            Codec.STRING.xmap(Long::parseLong, String::valueOf),
+            Codec.LONG
+    );
+
+    private static final Codec<Map<UUID, Long>> UUID_LONG_MAP_CODEC = Codec.unboundedMap(
+            Codec.STRING.xmap(UUID::fromString, UUID::toString),
+            Codec.LONG
+    );
+
+    private static final Codec<OfflineGrowthSavedData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            LONG_LONG_MAP_CODEC.optionalFieldOf(CHUNKS_TAG, Map.of()).forGetter(data -> data.chunkUnloadTimes),
+            UUID_LONG_MAP_CODEC.optionalFieldOf(ENTITIES_TAG, Map.of()).forGetter(data -> data.entityUnloadTimes)
+    ).apply(instance, OfflineGrowthSavedData::new));
+
+    public static final SavedDataType<OfflineGrowthSavedData> TYPE =
+            new SavedDataType<>(SAVE_ID, OfflineGrowthSavedData::new, CODEC, null);
 
     private final Map<Long, Long> chunkUnloadTimes = new HashMap<>();
     private final Map<UUID, Long> entityUnloadTimes = new HashMap<>();
 
-public static final SavedDataType<OfflineGrowthSavedData> TYPE =
-    new SavedDataType<>(SAVE_ID, OfflineGrowthSavedData::new, CODEC, DataFixTypes.LEVEL);
-
-public static OfflineGrowthSavedData get(ServerLevel level) {
-    return level.getDataStorage().computeIfAbsent(TYPE);
-}
-
-// OfflineGrowthSavedData.java
-private static OfflineGrowthSavedData load(CompoundTag tag) {
-    OfflineGrowthSavedData data = new OfflineGrowthSavedData();
-
-    CompoundTag chunksTag = tag.getCompoundOrEmpty(CHUNKS_TAG);
-    for (String key : chunksTag.keySet()) {
-        chunksTag.getLong(key).ifPresent(time ->
-            data.chunkUnloadTimes.put(Long.parseLong(key), time)
-        );
+    public OfflineGrowthSavedData() {
     }
 
-    ListTag entities = tag.getListOrEmpty(ENTITIES_TAG);
-    for (Tag raw : entities) {
-        if (!(raw instanceof CompoundTag entry)) continue;
-
-        var entityIdOpt = entry.getString(ENTITY_ID_TAG);
-        var timeOpt = entry.getLong(TIME_TAG);
-
-        if (entityIdOpt.isPresent() && timeOpt.isPresent()) {
-            UUID entityId = UUID.fromString(entityIdOpt.get());
-            data.entityUnloadTimes.put(entityId, timeOpt.get());
-        }
+    private OfflineGrowthSavedData(Map<Long, Long> chunkUnloadTimes, Map<UUID, Long> entityUnloadTimes) {
+        this.chunkUnloadTimes.putAll(chunkUnloadTimes);
+        this.entityUnloadTimes.putAll(entityUnloadTimes);
     }
 
-    return data;
-}
-
-    @Override
-    public CompoundTag save(CompoundTag tag, HolderLookup.Provider lookupProvider) {
-        CompoundTag chunksTag = new CompoundTag();
-        for (Map.Entry<Long, Long> entry : chunkUnloadTimes.entrySet()) {
-            chunksTag.putLong(Long.toString(entry.getKey()), entry.getValue());
-        }
-        tag.put(CHUNKS_TAG, chunksTag);
-
-        ListTag entities = new ListTag();
-        for (Map.Entry<UUID, Long> entry : entityUnloadTimes.entrySet()) {
-            CompoundTag entityTag = new CompoundTag();
-            entityTag.putString(ENTITY_ID_TAG, entry.getKey().toString());
-            entityTag.putLong(TIME_TAG, entry.getValue());
-            entities.add(entityTag);
-        }
-        tag.put(ENTITIES_TAG, entities);
-
-        return tag;
+    public static OfflineGrowthSavedData get(ServerLevel level) {
+        return level.getDataStorage().computeIfAbsent(TYPE);
     }
 
     public void recordChunkUnload(long chunkPos, long gameTime) {
